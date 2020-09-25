21621ffe17aed31a5e533f6a7f5eb5e03c5ed941
Szeifert Nóra <szeifert.nora@gmail.com>
2020-09-26 00:29:55 +0200

    Programkód

---
+++ main.py
@@ -0,0 +1,8 @@
+A = 3500    #út hossza(m)
+B = 9       #út szélessége(m)
+C = 0.12    #aszfalt vastagság(m)
+
+D = A*B*C   #szükséges aszfalt(m3)
+
+print("A szükséges aszfalt mennyiség: %d m3" % D)
+
